1954fad6e12f3a37fd905493196bff21ef370b66
YuliaK <yuka.gerasimenko@gmail.com>
2024-08-30 16:25:22 +0300

    homework done

--- HW7_1.js
+++ HW7_1.js
@@ -1,1 +1,14 @@
-
+const car = {
+    color: 'red',
+    brand: 'Toyota',
+    model: 'Corolla',
+        getInfo() {
+                for (let key in this) {
+                        if (this.hasOwnProperty(key) && typeof this[key] !== 'function') {
+                console.log(`${key}: ${this[key]}`);
+            }
+        }
+    }
+};
+car['type of fuel'] = 'Petrol';
+car.getInfo();
